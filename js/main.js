a4881a0b938eb17331430ba41a8e96ea14f12ed8
Dandd6541 <Mordkid4@gmail.com>
2022-07-03 12:49:53 -0700

    state (variables) updated

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@
 
 /*----- app's state (variables) -----*/
 //array of 42 elements DONE - MADE 42 DIV'S 
-
-
+//board which is the main part of the game
+//the turn (each player switches off going)
+// the game status (so where we are in the game and eventually, who WINS!)
 
 /*----- cached element references -----*/
 
