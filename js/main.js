d63c2f6c74456f87b13e809798752b40dcd933b7
Dandd6541 <Mordkid4@gmail.com>
2022-07-03 16:13:52 -0700

    added new variables

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@
 //board which is the main part of the game
 //the turn (each player switches off going)
 // the game status (so where we are in the game and eventually, who WINS!)
+let gameStatus; //talking about the current game play so in session is null, winner is 'W' and loser is 'L'
+let currentTurn; //this array hold the key for the current player whos turn it is
+let playerColor; //this indicates which player is which color and in our case P1=(yellow) 'Y' and P2=(red) 'R'
 
 /*----- cached element references -----*/
 
